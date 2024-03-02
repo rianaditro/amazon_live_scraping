ed0c2b1cb8c70b7f3bc5757902c6ca462ca9a416
rianaditro <rianaditro@gmail.com>
2024-03-02 15:51:05 +0700

    feat: add scraping.html

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint,render_template, request, redirect, url_for
+from scrap import main
 
 
 bp = Blueprint("main",__name__)
@@ -6,5 +7,12 @@ bp = Blueprint("main",__name__)
 @bp.route('/')
 def index():
     return render_template("index.html")
+
+@bp.route('/scraping',methods=["GET","POST"])
+def scraping():
+    keyword = request.form["keyword"]
+    items = main(keyword)
+
+    return render_template("/scraping.html", items=items)
 
 

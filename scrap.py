0942cc4e8c28ee7f4618ca2443b5713daff61f86
rianaditro <rianaditro@gmail.com>
2024-03-02 11:23:44 +0700

    feat: validate url

--- scrap.py
+++ scrap.py
@@ -3,13 +3,20 @@ from bs4 import BeautifulSoup
 import re
 
 def check_url(url:str)->str:
-    pattern = r"(.*?)/(?!.*?)"
-    match = re.search(pattern, text)
+    prefix = "https://www.amazon.com"
+    pattern = r"(/[^/]+/dp/[^/]+)/"
+    match = re.search(pattern, url)
 
     if match:
-        extracted_text = match.group(1)
+        shorten_url = match.group(1)
     else:
-        pass
+        shorten_url = url
+
+    if prefix not in shorten_url:
+        complete_url = prefix+shorten_url
+        return complete_url
+    else:
+        return shorten_url
 
 def find_tag_list(html:str)->list[BeautifulSoup]:
     soup = BeautifulSoup(html,"html.parser")
@@ -34,16 +41,19 @@ def parse(list_tag:list[BeautifulSoup]):
 
 
 if __name__=="__main__":
-    with open("httpx.html") as file:
-        html1 = file.read()
 
-    with open("httpx-boots.html") as file:
-        html2 = file.read()
+    url = "/Camera-Shy-Kay-Cove-ebook/dp/B0C7QYS91W/ref=sr_1_1?crid=85DM2WL0692G&dib=eyJ2IjoiMSJ9.Tf2Vy2x498pDapzMTCMs7EXp9X0hatZ_cD1qGsa3lOdKHyCbiTedaPIxOY2WBtGdq5MjKn_q_M1cGO51cubHX0z8a_elpJAjmHH_Cla6JpWn38FFrYuemzkR--WzePH7xj5V8twKPEZHqtV1Dj1R9KPIkgxOozzSeEhIrKY0CTSdLb4kiH1lIMcRlYSTiF-qVsiD8JN6z0SyisCI4bLUYbzE0T-l6qhbAclJjpIPRFw.bP2v4Ft8kwO2DaFIeXg_NaXq_p57YAyb5JVQeQGp7HM&dib_tag=se&keywords=camera&qid=1709346093&s=books&sprefix=camera%2Cstripbooks-intl-ship%2C317&sr=1-1"
+    print(check_url(url))
+    # with open("httpx.html") as file:
+    #     html1 = file.read()
 
-    tag1 = find_tag_list(html1)
-    print(parse(tag1))
+    # with open("httpx-boots.html") as file:
+    #     html2 = file.read()
 
-    tag2= find_tag_list(html2)
-    print(parse(tag2))
+    # tag1 = find_tag_list(html1)
+    # print(parse(tag1))
+
+    # tag2= find_tag_list(html2)
+    # print(parse(tag2))
     
     
